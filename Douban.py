56e52a506c0b6896298f1128b07405ad7d6a725b
nmerovingian <nmerovingian@gmail.com>
2020-12-03 17:58:29 +0800

    Updates

--- Douban.py
+++ Douban.py
@@ -17,11 +17,13 @@ from getpass import getpass
 
 
 class DouBan(object):
+    """
+    Used to login with Douban and maintain the session for request. 
+    """
     def __init__(self):
         self.login_url = 'https://accounts.douban.com/j/mobile/login/basic'
         self.headers = {
-            "User-Agent": ua.safari  #"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
-            ,#'cookie' : r'douban-fav-remind=1; _vwo_uuid_v2=D0EA24652E668F9059E333E4EBA7E0F0B|4113fd0dad106ebbed18eeb641145776; gr_user_id=c1273f03-0474-4cd8-9cd3-07849a4c6d05; bid=IM4E7IPnDN8; ll="108296"; viewed="34853568"; apiKey=; __utmz=30149280.1606972383.30.28.utmcsr=cn.bing.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmc=30149280; __utma=30149280.1529578497.1561719066.1606978260.1606981423.32; ap_v=0,6.0; _pk_ref.100001.2fad=%5B%22%22%2C%22%22%2C1606981444%2C%22https%3A%2F%2Fmovie.douban.com%2Fsubject%2F20376577%2Fcomments%3Fstart%3D300%26limit%3D20%26status%3DP%26sort%3Dnew_score%22%5D; _pk_ses.100001.2fad=*; __utmt=1; _pk_id.100001.2fad=692b942217e57c96.1606981444.1.1606982044.1606981444.; user_data={%22area_code%22:%22+86%22%2C%22number%22:%2218580731201%22%2C%22code%22:%223195%22}; vtoken=undefined; last_login_way=phone; push_noty_num=0; push_doumail_num=0; douban-profile-remind=1; __utmv=30149280.19461; __utmb=30149280.7.10.1606981423; login_start_time=1606982465670'
+            "User-Agent": ua.safari
         }
         self.login_data = {
             'ck': '',
@@ -80,6 +82,8 @@ def parse_comments(url,session,start,stop,max_comments):
             dates = s.xpath('//*[@id="comments"]/div/div[2]/h3/span[2]/span[3]/@title')
             #dates = [date.strip() for date in dates]
             scores = s.xpath('//*[@id="comments"]/div/div[2]/h3/span[2]/span[2]/@class')
+            scores = [score.replace('allstar','') for score in scores]
+            scores = [score.replace(' rating','')for score in scores]
             upvotes = s.xpath('//*[@id="comments"]/div/div[2]/h3/span[1]/span/text()')
             watched_already = s.xpath('//*[@id="comments"]/div/div[2]/h3/span[2]/span[1]/text()')
 
